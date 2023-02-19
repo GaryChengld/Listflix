630188cfe2e1b3d03da63511664069bbe2aa6747
GaryChengld <chengld@yahoo.com>
2023-02-19 11:16:41 -0500

    init commit for identity-service

---
+++ identity-service/src/main/java/com/gcsi/listflix/identity/IdentityServiceApp.java
@@ -0,0 +1,13 @@
+package com.gcsi.listflix.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApp.class, args);
+	}
+
+}
---
+++ identity-service/src/test/java/com/gcsi/listflix/identity/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcsi.listflix.identity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
