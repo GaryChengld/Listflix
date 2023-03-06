ac6335954ed97a7e3b177d41532b5df35d6c0622
GaryChengld <chengld@yahoo.com>
2023-03-06 08:55:27 -0500

    users entity

--- identity-service/src/main/java/com/gcsi/listflix/identity/IdentityServiceApp.java
+++ identity-service/src/main/java/com/gcsi/listflix/identity/IdentityServiceApp.java
@@ -3,6 +3,9 @@ package com.gcsi.listflix.identity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Gary Cheng
+ */
 @SpringBootApplication
 public class IdentityServiceApp {
 
