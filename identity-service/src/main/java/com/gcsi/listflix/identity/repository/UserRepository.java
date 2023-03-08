43678b4b98ef156edaed82ae742181d05345a1ba
GaryChengld <chengld@yahoo.com>
2023-03-07 22:32:29 -0500

    R2DBC

--- identity-service/src/main/java/com/gcsi/listflix/identity/repository/UserRepository.java
+++ identity-service/src/main/java/com/gcsi/listflix/identity/repository/UserRepository.java
@@ -1,23 +1,24 @@
 package com.gcsi.listflix.identity.repository;
 
 import com.gcsi.listflix.identity.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
 
 /**
  * @author Gary Cheng
  */
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends R2dbcRepository<User, Long> {
     /**
      * Find user by email.
      * @param email user's email
      * @return the user
      */
-    User findByEmail(String email);
+    Mono<User> findByEmail(String email);
 
     /**
      * Find user by username.
      * @param username user's username
      * @return the user
      */
-    User findByUsername(String username);
+    Mono<User> findByUsername(String username);
 }
