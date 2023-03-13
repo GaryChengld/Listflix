befba71de6a4d1015e03d10aaad48d1571dfe36a
GaryChengld <chengld@yahoo.com>
2023-03-12 21:54:27 -0400

    integrate flyway and r2dbc

--- identity-service/src/main/java/com/gcsi/listflix/identity/AppConfiguration.java
+++ identity-service/src/main/java/com/gcsi/listflix/identity/AppConfiguration.java
@@ -1,7 +1,10 @@
 package com.gcsi.listflix.identity;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 /**
@@ -11,5 +14,20 @@ import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 @EnableR2dbcAuditing
 @PropertySource("file:./env.properties")
 public class AppConfiguration {
+    private final Environment env;
 
+    public AppConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return new Flyway(Flyway.configure()
+                .baselineOnMigrate(true)
+                .dataSource(
+                        env.getRequiredProperty("spring.flyway.url"),
+                        env.getRequiredProperty("spring.flyway.user"),
+                        env.getRequiredProperty("spring.flyway.password"))
+        );
+    }
 }
--- identity-service/src/main/resources/db/migration/V1__init.sql
+++ identity-service/src/main/resources/db/migration/V1__init.sql
@@ -1,35 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-    id UUID DEFAULT uuid_generate_v4() GENERATED ALWAYS PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     username VARCHAR(255) UNIQUE NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
     password_hash varchar(255),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
-
-CREATE TABLE social_providers (
-    id SERIAL PRIMARY KEY,
-    provider_name VARCHAR(255) UNIQUE NOT NULL
-);
-
-CREATE TABLE social_accounts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    provider_id INTEGER REFERENCES social_providers(id) ON DELETE CASCADE,
-    user_reference_id VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE TABLE login_sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    token TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    expired_at TIMESTAMP NOT NULL,
-    source VARCHAR(255) NOT NULL
-);
-
-
